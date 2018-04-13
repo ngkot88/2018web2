ad19d8db9587c12ea9608c69657e7a409d0efa73
ngkot88 <37520436+ngkot88@users.noreply.github.com>
2018-04-13 16:12:06 +0900

    학교홈페이지 섹션 작업중, 이미지슬라이드 완료, 이미지 고르는중
    쀼

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -1,4 +1,19 @@
 $(document).ready(function () {
+
+   var num=1;
+
+   function slide_right(){
+      $(".box > img").eq(num).animate({left : -1000},0);
+      $(".box > img").eq(num-1).animate({left : 1000 ,opacity : 0},500);
+      $(".box > img").eq(num).animate({left : 0 ,opacity : 1},500);
+
+      num++;
+      if(num>=3){
+         num=0;
+      }
+   }
+   setInterval(slide_right,2000);
+
    $(".main_img").click(function () {
       $(".dog").css("display", "block");
    });
