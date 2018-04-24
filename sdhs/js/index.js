fe5d4b5e97736b6ea1c3a50d902b2d8f8bdffa23
ngkot88 <37520436+ngkot88@users.noreply.github.com>
2018-04-24 15:54:23 +0900

    학교홈페이지 테이블,footer 완료
    ㅎㅎㅎㅎ

--- sdhs/js/index.js
+++ sdhs/js/index.js
@@ -4,8 +4,8 @@ $(document).ready(function () {
 
    function slide_right(){
       $(".box > img").eq(num).animate({left : -1000},0);
-      $(".box > img").eq(num-1).animate({left : 1000 ,opacity : 0});
-      $(".box > img").eq(num).animate({left : 0 ,opacity : 1});
+      $(".box > img").eq(num-1).animate({left : 1000 ,opacity : 0},500);
+      $(".box > img").eq(num).animate({left : 0 ,opacity : 1},500);
 
       num++;
       if(num>=3){
@@ -22,7 +22,7 @@ $(document).ready(function () {
    });
 
    $(".sub_img_box").click(function () {
-       $(".sub_pup_box").css("display","block");
+       $(".sub_pup_box").css({"display" : "block"});
    });
    $(".sub_pup_box  button").click(function () {
       $(".sub_pup_box").css("display", "none");
@@ -33,5 +33,24 @@ $(document).ready(function () {
     });
     $(".food_pup_box button").click(function () {
         $(".food_pup_box").css("display", "none");
+    });
+
+    var dt = new Date();
+    var day = dt.getDate();
+
+    $(".now_date").text(day);
+
+    $(".notice_name").click(function () {
+        $(".notice_name").css({"color" : "#4171ff"});
+        $(".home_page").css({"color" : "#000000"});
+        $(".notice_table").css({"display" : "block"});
+        $(".home_table").css({"display" : "none"});
+
+    });
+    $(".home_page").click(function () {
+        $(".home_page").css({"color" : "#4171ff"});
+        $(".notice_name").css({"color" : "#000000"});
+        $(".notice_table").css({"display" : "none"});
+        $(".home_table").css({"display" : "block"});
     });
 });
